b5ef31bd69fa9305ffd478feb73c1787d938ae1d
aivazashvilitekla <tekla.aivazashvili@gau.edu.ge>
2022-01-25 17:24:17 +0400

    env

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+import path from "path";
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+import htmlWebpackPlugin from "html-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const config = {
+  entry: path.resolve(__dirname, "src", "index.js"),
+  mode: "development",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "./src/index.html",
+      inject: true,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
+export default config;
